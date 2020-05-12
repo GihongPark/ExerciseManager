f62a9678af0b9805807d632adb5294ac989ede6e
gipark <gipark1993@gmail.com>
2020-05-12 14:38:46 +0900

    Updates

---
+++ precache-manifest.e0394caaa1b3429d4fdaa7d73ca3ba96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33cbc0dab3ee742a43079baf18727c9",
+    "url": "/ExerciseManager/index.html"
+  },
+  {
+    "revision": "c61703e3dfc6fac2a083",
+    "url": "/ExerciseManager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aafa2a3234da047283dc",
+    "url": "/ExerciseManager/static/js/2.46fc69ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExerciseManager/static/js/2.46fc69ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61703e3dfc6fac2a083",
+    "url": "/ExerciseManager/static/js/main.d78f8d7c.chunk.js"
+  },
+  {
+    "revision": "b67b10051848cd420ae6",
+    "url": "/ExerciseManager/static/js/runtime-main.b63f7e90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ExerciseManager/static/media/logo.5d5d9eef.svg"
+  }
+]);
